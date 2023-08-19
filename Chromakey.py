bdb6c013e066d9f5d26e7060dc46c271c37c5b3f
imanoj997 <mbhatt997@gmail.com>
2023-08-19 23:31:30 +1000

    rgb to hsb upto splitting

--- Chromakey.py
+++ Chromakey.py
@@ -10,7 +10,7 @@ def check_image_exists(image_path):
     This function check if an image with a given path exists or not
 
     Args:
-    - image_path (str): full path of the image to be checked
+    - image (str): full path of the image to be checked
 
     Returns:
     - True of image exists, ends the program with a message if image doesn't exists.
@@ -21,17 +21,36 @@ def check_image_exists(image_path):
         print(f"Error: {image_path} does not exist.")
         exit()
 
-def display_image(image_path):
-    # Read the image
-    image = cv.imread(image_path)
+def rgb_to_hsb(img):
+    """
+    This function converts RGB image to HSB color spaces
+    and displays it in a collage of original image alongside
+    3 grayscale versions of each band of HSB color spaces.
 
-    # Check if image was successfully loaded
-    if image is None:
-        print("Error loading image")
-        return
+    Args:
+    - img (cv2:img): image object of cv2
 
+    Returns:
+    - None
+    """
     # Display the image`ß
-    cv.imshow('Image Display', image)
+    cv.imshow("Original Image Display", img)
+
+    hsb_image = cv.cvtColor(img, cv.COLOR_BGR2HSV_FULL)
+    # cv.imshow("hsb",hsb_image)
+
+    # split the hsb image into individual channels
+    h,s,b = cv.split(hsb_image)
+
+    # Stack images horizontally
+    top_row = np.hstack([img, b])
+    bottom_row = np.hstack([h, s])
+
+    # Stack images vertically
+    final_img = np.vstack([top_row, bottom_row])
+
+    # Display the final image
+    cv.imshow('Four Images', final_img)
 
     # Wait for a key press and then close the image window
     cv.waitKey(0)
@@ -54,17 +73,24 @@ def main():
         # Extract the image path
         image_path = sys.argv[2]
         check_image_exists(image_path)
-        # rgb_to_hsb()
+        # Read the image
+        image = cv.imread(image_path)
+
+        # Check if image was successfully loaded
+        if image is None:
+            print("Error loading image")
+            exit()
+        else:
+            rgb_to_hsb(image)
+            pass
+
     # code for task 2
     else:
         #runtask2
         print("task 2-------------")
         return
 
-    # Display the image
-    display_image(image_path)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # start python program execution
     main()
 
